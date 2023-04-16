bef141627f2d10ffd8e42a8a58875a936a408d35
Kevin Christanto <kevinchrist75@gmail.com>
2023-04-16 12:35:56 +0700

    added songs image
    next todo: fix nav back button + added CTA under the list of songs

--- Constants.swift
+++ Constants.swift
@@ -15,44 +15,45 @@ let kSadSongs: [Songs] = [
     //masukin lagu dan image lagu2 sad
 
     Songs(title: "Mantan Terindah", artist: "Kahitna", image: Image("song-1")),
-    Songs(title: "Separuh Aku", artist: "Noah", image: Image("song-1")),
-    Songs(title: "Kangen", artist: "Dewa 19", image: Image("song-1")),
-    Songs(title: "Hampa", artist: "Ari Lasso", image: Image("song-1")),
-    Songs(title: "Lumpuhkan Ingatanku", artist: "Geisha", image: Image("song-1")),
-    Songs(title: "Peri Cintaku", artist: "Ziva Magnolya", image: Image("song-1")),
-    Songs(title: "Pesan Terakhir", artist: "Lyodra", image: Image("song-1")),
-    Songs(title: "Tak Ingin Usai Cintaku", artist: "Ziva", image: Image("song-1")),
-    Songs(title: "Komang", artist: "Raim Laode", image: Image("song-1")),
-    Songs(title: "Langit Abu-abu", artist: "Tulus", image: Image("song-1"))
+    Songs(title: "Separuh Aku", artist: "Noah", image: Image("song-2")),
+    Songs(title: "Kangen", artist: "Dewa 19", image: Image("song-3")),
+    Songs(title: "Hampa", artist: "Ari Lasso", image: Image("song-4")),
+    Songs(title: "Langit Abu-abu", artist: "Tulus", image: Image("song-5")),
+    Songs(title: "Lumpuhkan Ingatanku", artist: "Geisha", image: Image("song-6")),
+    Songs(title: "Peri Cintaku", artist: "Ziva Magnolya", image: Image("song-7")),
+    Songs(title: "Pesan Terakhir", artist: "Lyodra", image: Image("song-8")),
+    Songs(title: "Tak Ingin Usai ", artist: "Keisya Levronkaa", image: Image("song-9")),
+    Songs(title: "Komang", artist: "Raim Laode", image: Image("song-10")),
+
 ]
 
 let kRelaxedSongs: [Songs] = [
     //masukin lagu dan image lagu2 relax
     
-    Songs(title: "Akad", artist: "Payung Teduh", image: Image("song-1")),
-    Songs(title: "Malam Mingguku", artist: "Yovie & Nuno", image: Image("song-1")),
-    Songs(title: "Happy", artist: "Skinnyfabs", image: Image("song-1")),
-    Songs(title: "Kesempurnaan Cinta", artist: "Rizky Febian", image: Image("song-1")),
-    Songs(title: "Diatas Meja", artist: "Payung Teduh", image: Image("song-1")),
-    Songs(title: "Cinta Luar Biasa", artist: "Andmesh", image: Image("song-1")),
-    Songs(title: "Sempurna", artist: "Andra and The Backbone", image: Image("song-1")),
-    Songs(title: "Hati-hati di Jalan", artist: "Tulus", image: Image("song-1")),
-    Songs(title: "Cinta Dalam Hati", artist: "Ungu", image: Image("song-1")),
-    Songs(title: "Teman Hidup Mu", artist: "Tulus", image: Image("song-1"))
+    Songs(title: "Akad", artist: "Payung Teduh", image: Image("song-11")),
+    Songs(title: "Malam Mingguku", artist: "Yovie & Nuno", image: Image("song-12")),
+    Songs(title: "Happy", artist: "Skinnyfabs", image: Image("song-13")),
+    Songs(title: "Kesempurnaan Cinta", artist: "Rizky Febian", image: Image("song-14")),
+    Songs(title: "Diatas Meja", artist: "Payung Teduh", image: Image("song-15")),
+    Songs(title: "Cinta Luar Biasa", artist: "Andmesh", image: Image("song-16")),
+    Songs(title: "Sempurna", artist: "Andra and The Backbone", image: Image("song-17")),
+    Songs(title: "Hati-hati di Jalan", artist: "Tulus", image: Image("song-18")),
+    Songs(title: "Cinta Dalam Hati", artist: "Ungu", image: Image("song-19")),
+    Songs(title: "Teman Hidup", artist: "Tulus", image: Image("song-10"))
 ]
 
 let kHappySongs: [Songs] = [
     //masukin lagu dan image lagu2 happy
     
-    Songs(title: "Lebih Indah", artist: "Adera", image: Image("song-1")),
-    Songs(title: "Laskar Pelangi", artist: "Nidji", image: Image("song-1")),
-    Songs(title: "Dan", artist: "Sheila on 7", image: Image("song-1")),
-    Songs(title: "Melompat Lebih Tinggi", artist: "Sheila on 7", image: Image("song-1")),
-    Songs(title: "Doremi", artist: "Budi Doremi", image: Image("song-1")),
-    Songs(title: "Jadi Gila", artist: "RAN", image: Image("song-1")),
-    Songs(title: "Selamat Pagi", artist: "RAN", image: Image("song-1")),
-    Songs(title: "Dia", artist: "Maliq D'Essentials", image: Image("song-1")),
-    Songs(title: "Adu Rayu", artist: "Tulus", image: Image("song-1")),
-    Songs(title: "Manusia Kuat", artist: "Tulus", image: Image("song-1"))
+    Songs(title: "Lebih Indah", artist: "Adera", image: Image("song-21")),
+    Songs(title: "Laskar Pelangi", artist: "Nidji", image: Image("song-22")),
+    Songs(title: "Dan", artist: "Sheila on 7", image: Image("song-23")),
+    Songs(title: "Melompat Lebih Tinggi", artist: "Sheila on 7", image: Image("song-24")),
+    Songs(title: "Doremi", artist: "Budi Doremi", image: Image("song-25")),
+    Songs(title: "Jadi Gila", artist: "RAN", image: Image("song-26")),
+    Songs(title: "Selamat Pagi", artist: "RAN", image: Image("song-27")),
+    Songs(title: "Dia", artist: "Maliq D'Essentials", image: Image("song-28")),
+    Songs(title: "Adu Rayu", artist: "Yovie, Tulus, Glenn", image: Image("song-29")),
+    Songs(title: "Manusia Kuat", artist: "Tulus", image: Image("song-30"))
 ]
 
